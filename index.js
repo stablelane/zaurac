62064352170bfe7d7a8058caaa1923d1ec105772
stablelane <aaanshad09@gmail.com>
2024-10-24 22:09:21 +0530

    add nav function

---
+++ index.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links a');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navLinks.classList.remove('active');
+        });
+    });
