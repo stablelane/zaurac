0fc4b6c661a872aef7bc54cdbde36bd1c8e339cc
stablelane <aaanshad09@gmail.com>
2024-10-31 13:19:32 +0530

    add hamburger menu function to blog page

--- blog.js
+++ blog.js
@@ -1,3 +1,21 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links a');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+    document.getElementById('container').classList.toggle('shadow')
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navLinks.classList.remove('active');
+        document.getElementById('container').classList.remove('shadow')
+    });
+});
+
 // Store blog posts data
 const blogPosts = [
     {
